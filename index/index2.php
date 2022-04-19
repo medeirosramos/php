161e040fbe8062dcd708746ac84fd0e475b209f7
Rodrigo Medeiros <44650854+medeirosramos@users.noreply.github.com>
2022-04-19 16:48:28 -0300

    validacoes html5

---
+++ index/index2.php
@@ -0,0 +1,12 @@
+<?php
+print "<!DOCTYPE html>";
+print "<html lang='pt-br'>";
+print "    <head>";
+print "        <meta charset='utf-8'>";
+print "        <title>Teste Html 5</title>";
+print "    </head>";
+print "    <body>";
+print "        Data: <input type='date' name='data'>";
+print "    </body>";
+print "</html>";
+?>
