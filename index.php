957e0d1b1bea16e0a4339a09d47f4990085e5673
Rodrigo Medeiros <44650854+medeirosramos@users.noreply.github.com>
2022-11-29 17:31:08 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,13 @@
-<?php phpinfo(); ?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Exemplo</title>
+    </head>
+    <body>
+
+        <?php
+            echo "Olá, eu sou um script PHP!";
+        ?>
+      <h3>Documentação PHP: <a href=https://www.php.net/manual/pt_BR/indexes.examples.php>EXEMPLOS</a></h3>
+    </body>
+</html>
