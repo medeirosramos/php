7d8142ae366ed9c71549f7faa11f0a2b71efd3c4
Rodrigo Medeiros <44650854+medeirosramos@users.noreply.github.com>
2022-11-29 17:26:54 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,1 @@
-<?php
-print "<!DOCTYPE html>";
-print "<html lang='pt-br'>";
-print "    <head>";
-print "        <meta charset='utf-8'>";
-print "        <title>Index PHP with Html 5</title>";
-
-print "    </head>";
-print "    <body>";
-print "        Data: <input type='date' name='data'>";
-print " <br><a href=info.php>info</a>"
-print "    </body>";
-print "</html>";
-?>
+<?php phpinfo(); ?>
